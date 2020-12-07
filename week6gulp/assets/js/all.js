ba2ad0502090ed21c75c08942679b5e3867d39f3
peggyted0129 <peggyted0129@gamil.com>
2020-12-07 22:27:20 +0800

    updatejs

--- week6gulp/assets/js/all.js
+++ week6gulp/assets/js/all.js
@@ -4,13 +4,9 @@ console.log('Hello!'); // AOS 動畫
 
 AOS.init({
   offset: 200,
-  // offset (in px) from the original trigger point
   delay: 0,
-  // values from 0 to 3000, with step 50ms
-  duration: 1000 // values from 0 to 3000, with step 50ms
-
-}); // AOS套件
-// RWD navbar 首頁
+  duration: 1000
+}); // RWD navbar 首頁
 
 $(document).ready(function () {
   console.log('HesSchool Hello!');
@@ -54,5 +50,8 @@ $(document).ready(function () {
 
     ;
   });
-});
+}); // parallax
+
+var scene = document.getElementById('scene');
+var parallaxInstance = new Parallax(scene);
 //# sourceMappingURL=all.js.map
