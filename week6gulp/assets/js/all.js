0d3d88e5b54fa784067232c118428a5c2c7225d4
peggyted0129 <peggyted0129@gamil.com>
2020-12-26 00:05:33 +0800

    1226

--- week6gulp/assets/js/all.js
+++ week6gulp/assets/js/all.js
@@ -1,57 +1,51 @@
-"use strict";
+console.log('Hello!');
 
-console.log('Hello!'); // AOS 動畫
-
+// AOS 動畫
 AOS.init({
-  offset: 200,
+  offset: 200, 
   delay: 0,
-  duration: 1000
-}); // RWD navbar 首頁
+  duration: 1000, 
+}); 
 
-$(document).ready(function () {
+// RWD navbar 首頁
+$(document).ready(function() {
   console.log('HesSchool Hello!');
-  $(".navbar-toggler").on('click', function () {
-    $(".toggler-bar-top").toggleClass("toggler-bar-top-rotate");
-    $(".toggler-bar-middle").toggleClass("d-none");
-    $(".toggler-bar-bottom").toggleClass("toggler-bar-bottom-rotate");
-  });
-}); // declare variable
+    $(".navbar-toggler").on('click',(function(){
+      $(".toggler-bar-top").toggleClass("toggler-bar-top-rotate");
+      $(".toggler-bar-middle").toggleClass("d-none");
+      $(".toggler-bar-bottom").toggleClass("toggler-bar-bottom-rotate");
+    }))
+})
 
+// declare variable
 var scrollTop = $(".scroll-top");
-$(window).scroll(function () {
-  var topPos = $(this).scrollTop();
 
+$(window).scroll(function() {
+  var topPos = $(this).scrollTop();
   if (topPos > 100) {
     $(".scroll-top").css("opacity", "1");
   } else {
     $(".scroll-top").css("opacity", "0");
   }
 }); // scroll END
-//Click event to scroll to top
 
-$(".scroll-top").click(function () {
+//Click event to scroll to top
+$(".scroll-top").click(function() {
   $('html, body').animate({
     scrollTop: 0
   }, 800);
   return false;
+
 }); // click() scroll top EMD
 
-$(document).ready(function () {
-  $(window).scroll(function () {
-    if ($(window).scrollTop() > 685) {
-      $('#navbarShow').addClass('sticky-top');
-    }
+$(document).ready(function(){
+  $(window).scroll (function() {
+      if ($(window).scrollTop()>685) {
+          $('#navbarShow').addClass('sticky-top');
+      };
+      if ($(window).scrollTop()<686) {
+          $('#navbarShow').removeClass('sticky-top');
+      };
+  })
+})
 
-    ;
-
-    if ($(window).scrollTop() < 686) {
-      $('#navbarShow').removeClass('sticky-top');
-    }
-
-    ;
-  });
-}); // parallax
-
-var scene = document.getElementById('scene');
-var parallaxInstance = new Parallax(scene);
-//# sourceMappingURL=all.js.map
